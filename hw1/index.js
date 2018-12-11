2c8b44a3dbf919403f2fc74b5e8c83479b6bbb6b
fraser71 <fraser71@users.noreply.github.com>
2018-12-11 11:54:10 +0200

    Return JSON to complete assignment

--- hw1/index.js
+++ hw1/index.js
@@ -7,21 +7,24 @@
 var http = require('http');
 var url = require('url');
 
-var server = http.createServer(function(req, res){
-    var urlDetails = url.parse(req.url);
+var server = http.createServer(function (req, res) {
+  var urlDetails = url.parse(req.url);
 
-    var path = urlDetails.pathname.replace(/^\/|\/$/g,'');
-    if (path == "hello"){
-        res.end("Homework 1 is done, World.")
-    }else{
-        res.statusCode = 404;
-        res.end();
-    }
-    
+  var path = urlDetails.pathname.replace(/^\/|\/$/g, '');
+  if (path == "hello") {
+
+    res.setHeader('content-type', 'application/json');
+    payloadstring = JSON.stringify({ message: "Homework 1 is done, World." }); 
+    res.end(payloadstring);
+  
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
+
 });
 
 var httpPort = 3000;
-server.listen(httpPort,function(){
-    console.log("Server is started and listening on port " + httpPort);
+server.listen(httpPort, function () {
+  console.log("Server is started and listening on port " + httpPort);
 });
-
